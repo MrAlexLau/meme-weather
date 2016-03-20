5f2fe7a09a9e726ba4ce25089c11e79c0a00b8af
Alexander Lau <laux@Alexanders-MacBook-Pro-2.local>
2016-03-20 18:55:35 -0500

    Add spec for when the search box is empty

--- spec/features/visit_homepage_spec.rb
+++ spec/features/visit_homepage_spec.rb
@@ -10,4 +10,12 @@ feature "Looking up the weather" do
       expect(page).to have_text("Go San Jose is 64.24° F and condtions are scattered clouds")
     end
   end
+
+  scenario "User does not enter a location" do
+    visit root_path
+    fill_in "location", :with => ""
+    click_button "Go"
+
+    expect(page).to have_text("Please enter a location.")
+  end
 end
